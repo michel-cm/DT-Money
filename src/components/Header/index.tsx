3a68cc7b8c98394779559279c3b208a0a2484e61
Michel Correa <58608300+michel-cm@users.noreply.github.com>
2023-07-28 13:58:09 -0300

    add radix-ui Modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import { HeaderContainer, HeaderContent, NewTransationButton } from "./styles";
+import * as Dialog from "@radix-ui/react-dialog";
 
 import logo from "../../assets/logo.svg";
 
@@ -8,7 +9,21 @@ export function Header() {
       <HeaderContent>
         <img src={logo} />
 
-        <NewTransationButton>Nova Transação</NewTransationButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransationButton>Nova Transação</NewTransationButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
+
       </HeaderContent>
     </HeaderContainer>
   );
